27a6c72c1540fc05bbc30e49bcf638c53ad4254e
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-01-07 20:35:31 +0100

    Automatic Angular Push

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // {
+  //   path: 'home',
+  //   component: HomePageComponent
+  // },
+  // {
+  //   path: 'about',
+  //   component: AboutPageComponent
+  // },
+  // {
+  //   path: '**',
+  //   redirectTo: 'home'
+  // }
+];
---
+++ src/app/auth/pages/layout-page/layout-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-page',
+  imports: [],
+  templateUrl: './layout-page.html',
+  styles: ``,
+})
+export class LayoutPage {
+
+}
