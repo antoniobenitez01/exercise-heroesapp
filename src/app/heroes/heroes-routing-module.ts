bffa16be4ce712e42bc421e4aa97c78177752898
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-01-07 21:08:09 +0100

    Automatic Angular Push

--- src/app/heroes/heroes-routing-module.ts
+++ src/app/heroes/heroes-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    //  locahost:4200/heroes/
+    path: '',
+    component: LayoutPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
