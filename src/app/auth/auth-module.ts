dbe0d3cbb0b252117fd2561ceec6f627cb9847b2
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-01-07 20:45:36 +0100

    Automatic Angular Push

--- src/app/auth/auth-module.ts
+++ src/app/auth/auth-module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing-module';
+import { LayoutPageComponent } from './pages/layout-page/layout-page';
+import { LoginPageComponent } from './pages/login-page/login-page';
+import { RegisterPageComponent } from './pages/register-page/register-page';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutPageComponent,
+    LoginPageComponent,
+    RegisterPageComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
--- src/app/heroes/heroes-module.ts
+++ src/app/heroes/heroes-module.ts
@@ -2,10 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing-module';
+import { HeroPageComponent } from './pages/hero-page/hero-page';
+import { LayoutPageComponent } from './pages/layout-page/layout-page';
+import { ListPageComponent } from './pages/list-page/list-page';
+import { NewPageComponent } from './pages/new-page/new-page';
+import { SearchPageComponent } from './pages/search-page/search-page';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
+  ],
   imports: [
     CommonModule,
     HeroesRoutingModule
