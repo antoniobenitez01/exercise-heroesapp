f736b7f2b89929bff80cd878040e10f1204cc87b
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-01-21 21:06:34 +0100

    Automatic Angular Push

--- src/app/heroes/services/heroes.ts
+++ src/app/heroes/services/heroes.ts
@@ -15,7 +15,7 @@ export class HeroesService
   }
 
   getHeroById(id : string) : Observable<Hero | undefined> {
-    return this.http.get<Hero>(`${this.baseUrl}/heroe/${id}`)
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
     .pipe( catchError( error => of(undefined)));
   }
 }
