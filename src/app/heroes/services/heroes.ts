8535c15653e31f72a55d35c93fcf37a6ce7b2e2a
antoniobenitez01 <anthonybenitezcontact@gmail.com>
2026-01-21 21:03:56 +0100

    Automatic Angular Push

--- src/app/heroes/services/heroes.ts
+++ src/app/heroes/services/heroes.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Hero } from '../interfaces/hero-interface';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 
 @Injectable({providedIn: 'root'})
@@ -14,4 +14,8 @@ export class HeroesService
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
   }
 
+  getHeroById(id : string) : Observable<Hero | undefined> {
+    return this.http.get<Hero>(`${this.baseUrl}/heroe/${id}`)
+    .pipe( catchError( error => of(undefined)));
+  }
 }
